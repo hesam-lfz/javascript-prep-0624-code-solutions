10b24fdac6787e73dcba236680c0979a7fc827d1
Hesam Samimi <hesam.samimi.cs@gmail.com>
2024-06-27 17:21:17 -0700

    Completed prep-javascript-functions exercise.

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,6 @@
+function getCircleCircumference1(r) { return 2 * r * Math.PI }
+
+const getCircleCircumference2 = r => 2 * r * Math.PI
+
+console.log(getCircleCircumference1(10))
+console.log(getCircleCircumference2(10))
